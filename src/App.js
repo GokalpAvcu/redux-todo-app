b7cad384fb99b9e05fb9acdf708fb03bbf776685
Gökalp Avcu <gokalp.avcu@santsg.com>
2022-09-26 18:15:31 +0300

    add changes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import "./App.css";
-import  Header from "./components/Header";
-import  Main from "./components/Main";
+import Header from "./components/Header";
+import Content from "./components/Content";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <section className="todoapp">
-      <Header />
-      <Main/>
-      
-    </section>
+    <>
+      <section className="todoapp">
+        <Header />
+        <Content />
+      </section>
+
+      <Footer />
+    </>
   );
 }
 
