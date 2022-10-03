0fd130870d55b95dcb5ba015c3314db41c4115ed
Gökalp Avcu <avcu219@gmail.com>
2022-10-03 13:46:43 +0300

    test

--- src/redux/todos/todosSlice.js
+++ src/redux/todos/todosSlice.js
@@ -4,6 +4,7 @@ import { createSlice } from "@reduxjs/toolkit";
 export const todosSlice = createSlice({
   name: "todos",
   initialState: {
+    dafd: "asdasd",
     items: [
       {
         id: "1",
