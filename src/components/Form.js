ce908025f59339f9605c7b4656020e6791c80475
Gökalp Avcu <gokalp.avcu@santsg.com>
2022-10-03 13:43:30 +0300

    add todos

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,13 +1,29 @@
-import React from "react";
-import {useDispatch} from "react-redux"
+import React, { useState } from "react";
+
+import { useDispatch } from "react-redux";
+import {nanoid} from "@reduxjs/toolkit";
+import { addTodo } from "../redux/todos/todosSlice";
 
 function Form() {
+  const [title, setTitle] = useState(''); 
+
+  const dispatch = useDispatch(); // dispatch fonksiyonunu çağırdık.
+
+  const handleSubmit = (e) => { // form submit olduğunda çalışacak fonksiyon
+    e.preventDefault(); // sayfa yenilenmesini engellemek için
+
+    dispatch(addTodo({ id: nanoid(), title, completed: false })) // dispatch fonksiyonu ile action'ı çağırdık. action'ın içindeki payload'ı da gönderdik."}))
+    setTitle('') // title'ı boşalttık.
+  };
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}> {/* form submit olduğunda handleSubmit fonksiyonunu çalıştır */}
       <input
         className="new-todo"
         placeholder="What needs to be done?"
         autoFocus
+        value={title} // input'un value'su title
+        onChange={(e) => setTitle(e.target.value)} // input'a her yazıldığında title'ı güncelle
       />
     </form>
   );
