516fbf1c413b07b616489de4f2775d682b955fdb
Gökalp Avcu <avcu219@gmail.com>
2022-10-03 15:46:21 +0300

    add changes

--- src/redux/todos/todosSlice.js
+++ src/redux/todos/todosSlice.js
@@ -20,10 +20,16 @@ export const todosSlice = createSlice({
   },
   reducers: {
     addTodo: (state,action) => {
-      state.items.push(action.payload)
+      state.items.push(action.payload) // action.payload => src\components\AddTodo.js'de tanımladığımız addTodo({title: title, completed: false})'un title kısmını alıyorum.
+    },
+    toggle: (state,action) => {
+    const {id} = action.payload //  action.payload => src\components\TodoList.js'de tanımladığımız toggle(item.id)'in item.id kısmını alıyorum.
+
+    const item = state.items.find((item) => item.id === id); // item.id === id => item.id'yi id ile karşılaştırıyorum. eğer item.id id'ye eşitse item'ı döndürüyorum.
+    item.completed = !item.completed; // true ise false, false ise true yapmak için ! kullanıyorum.
     }
   },
 });
 
-export const {addTodo} = todosSlice.actions;
+export const {addTodo} = todosSlice.actions; 
 export default todosSlice.reducer; // reducer'ı dışarı aktardık çünkü bunu store'da import edip reducer field'ına vermemiz gerekiyor.
