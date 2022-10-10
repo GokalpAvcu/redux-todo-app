c882a22189a04b4ccdac72f781b004b470755953
Gökalp Avcu <avcu219@gmail.com>
2022-10-10 15:12:56 +0300

    add changes

--- src/components/ContentFooter.js
+++ src/components/ContentFooter.js
@@ -1,10 +1,17 @@
 import React from "react";
 
+import {useSelector} from 'react-redux';
+
 function ContentFooter() {
+
+  const items = useSelector((state) => state.todos.items); // state.todos.items => src\redux\todos\todosSlice.js'de tanımladığımız initialState'in items kısmını alıyorum.
+  const itemsLeft = items.filter(item => !item.completed).length; // item.completed => src\redux\todos\todosSlice.js'de tanımladığımız initialState'in items kısmındaki completed kısmını alıyorum. eğer completed false ise item'ı döndürüyorum. length => item'ların sayısını alıyorum.
+  
+
   return (
     <footer className="footer">
       <span className="todo-count">
-        <strong>2</strong>  {"items left"}
+        <strong>{itemsLeft}</strong>  {"items left"}
         
       </span>
 
