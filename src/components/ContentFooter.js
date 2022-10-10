59d6cac5163eaa82eea3962e1539ebdb8abbcfe3
Gökalp Avcu <avcu219@gmail.com>
2022-10-10 15:34:08 +0300

    add changes

--- src/components/ContentFooter.js
+++ src/components/ContentFooter.js
@@ -1,31 +1,33 @@
 import React from "react";
 
-import {useSelector} from 'react-redux';
+import { useSelector } from "react-redux";
 
 function ContentFooter() {
-
   const items = useSelector((state) => state.todos.items); // state.todos.items => src\redux\todos\todosSlice.js'de tanımladığımız initialState'in items kısmını alıyorum.
-  const itemsLeft = items.filter(item => !item.completed).length; // item.completed => src\redux\todos\todosSlice.js'de tanımladığımız initialState'in items kısmındaki completed kısmını alıyorum. eğer completed false ise item'ı döndürüyorum. length => item'ların sayısını alıyorum.
-  
+  const itemsLeft = items.filter((item) => !item.completed).length; // item.completed => src\redux\todos\todosSlice.js'de tanımladığımız initialState'in items kısmındaki completed kısmını alıyorum. eğer completed false ise item'ı döndürüyorum. length => item'ların sayısını alıyorum.
+  const activeFilter = useSelector((state) => state.todos.activeFilter);
 
   return (
     <footer className="footer">
       <span className="todo-count">
-        <strong>{itemsLeft}</strong>  {"items left"}
-        
+        <strong>{itemsLeft}</strong> item{itemsLeft > 1 && "s"} left
       </span>
 
       <ul className="filters">
         <li>
-          <a href="#/" className="selected">
+          <a href="#/" className={activeFilter === "all" ? "selected" : ""}>
             All
           </a>
         </li>
         <li>
-          <a href="#/">Active</a>
+          <a href="#/" className={activeFilter === "all" ? "selected" : ""}>
+            Active
+          </a>
         </li>
         <li>
-          <a href="#/">Completed</a>
+          <a href="#/" className={activeFilter === "all" ? "selected" : ""}>
+            Completed
+          </a>
         </li>
       </ul>
 
